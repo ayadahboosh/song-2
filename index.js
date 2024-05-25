9e323943323f964a66231ce2aa55863d397b1112
Aya Ahmed <ayaahmeed963@gmail.com>
2024-05-25 13:09:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function displaySongInfo() {
+   
+    const songName = document.getElementById('songName').value;
+    const youtubeLink = document.getElementById('youtubeLink').value;
+    const description = document.getElementById('description').value;
+    const year = document.getElementById('year').value;
+
+    // Convert YouTube link to embeddable format "seach on it from youtube"
+    const embeddableLink = embedLink(youtubeLink);
+
+    document.getElementById('displaySongName').textContent = songName;
+    document.getElementById('displayDescription').textContent = description;
+    document.getElementById('displayYear').textContent = year;
+
+    const videoContainer = document.getElementById('videoContainer');
+    videoContainer.innerHTML = `<iframe src="${embeddableLink}"  ></iframe>`;
+
+    document.getElementById('formarea').style.display = 'none';
+    document.getElementById('display-area').style.display = 'block';
+
+    }
+
+function embedLink(youtubeLink) {
+    const videolink = youtubeLink.split('video=');
+    const embedLink = `https://www.youtube.com/embed/${videolink}`;
+    return embedLink;
+}
